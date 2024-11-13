7eab3b863647085c87eddf922f24ea09da7ef222
Bengt Söderberg <bengt1982@gmail.com>
2024-11-13 11:54:01 +0100

    Add initial SoundManager

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    private static SoundManager _instance;
+
+    private AudioSource _audioSource;
+
+    private void Awake()
+    {
+        if (_instance == null && _instance != this)
+        {
+            _instance = this;
+            _audioSource = GetComponent<AudioSource>();
+            DontDestroyOnLoad(_instance);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
