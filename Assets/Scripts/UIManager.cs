3d78fa64a12ca9d8088edff9cd120f8f2a5631a9
Bengt Söderberg <bengt1982@gmail.com>
2024-11-14 11:03:29 +0100

    Update heat meter

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -3,16 +3,14 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    private GameObject _heatBar;
+    private Image _heatBar;
     private HeatSystem _heatSystem;
 
     private void Start()
     {
-        _heatBar = GameObject.Find("HeatBar");
+        _heatBar = GameObject.Find("HeatBar").GetComponent<Image>();
         _heatSystem = FindObjectOfType<HeatSystem>();
 
-        _heatBar.GetComponent<Slider>().maxValue = HeatSystem.MaximumHeat;
-
-        _heatSystem.HeatChanged += heat => { _heatBar.GetComponent<Slider>().value = heat; };
+        _heatSystem.HeatChanged += heat => _heatBar.fillAmount = heat;
     }
 }
