45c7151edf0270df96e4fe553b7a3b0cc32c6d51
Bengt Söderberg <bengt1982@gmail.com>
2024-11-15 13:13:19 +0100

    Add won and lost screens

--- Assets/Scripts/UserInterface.cs
+++ Assets/Scripts/UserInterface.cs
@@ -1,4 +1,3 @@
-using System;
 using Managers;
 using Player;
 using UnityEngine;
@@ -6,22 +5,39 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    private GameObject _pauseMenu;
+    private GameObject _lostMenu;
+    private GameObject _wonMenu;
+
     private Image _heatBar;
-    private GameObject _popup;
     private HeatSystem _heatSystem;
 
 
     private void Awake()
     {
-        GameObject.Find("RestartBtn").GetComponent<Button>().onClick.AddListener(() =>
-        {
-            Time.timeScale = 1;
-            MasokingSceneManager.Restart();
-        });
-        GameObject.Find("QuitBtn").GetComponent<Button>().onClick.AddListener(MasokingSceneManager.Quit);
+        _pauseMenu = GameObject.Find("PauseMenu");
+        _pauseMenu.transform.Find("Panel/RestartBtn").GetComponent<Button>().onClick.AddListener(Restart);
+        _pauseMenu.transform.Find("Panel/QuitBtn").GetComponent<Button>().onClick.AddListener(Quit);
+        _pauseMenu.SetActive(false);
 
-        _popup = GameObject.Find("Popup");
-        _popup.SetActive(false);
+        _lostMenu = GameObject.Find("LostMenu");
+        _lostMenu.transform.Find("Panel/RestartBtn").GetComponent<Button>().onClick.AddListener(Restart);
+        _lostMenu.SetActive(false);
+
+        _wonMenu = GameObject.Find("WonMenu");
+        _wonMenu.transform.Find("Panel/QuitBtn").GetComponent<Button>().onClick.AddListener(Quit);
+        _wonMenu.SetActive(false);
+    }
+
+    private static void Restart()
+    {
+        Time.timeScale = 1;
+        MasokingSceneManager.Restart();
+    }
+
+    private static void Quit()
+    {
+        MasokingSceneManager.Quit();
     }
 
     private void Start()
@@ -30,16 +46,27 @@ public class UIManager : MonoBehaviour
         _heatSystem = FindObjectOfType<HeatSystem>();
 
         _heatSystem.HeatChanged += heat => _heatBar.fillAmount = heat;
-        _heatSystem.HeatMaxedOut += () => { };
-        _heatSystem.HeatDepleted += () => { };
+        _heatSystem.HeatMaxedOut += () =>
+        {
+            Time.timeScale = 1;
+            _wonMenu.SetActive(true);
+        };
+        _heatSystem.HeatDepleted += () =>
+        {
+            Time.timeScale = 1;
+            _lostMenu.SetActive(true);
+        };
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (!_lostMenu.activeSelf && !_wonMenu.activeSelf)
         {
-            Time.timeScale = _popup.activeSelf ? 1 : 0;
-            _popup.SetActive(!_popup.activeSelf);
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Time.timeScale = _pauseMenu.activeSelf ? 1 : 0;
+                _pauseMenu.SetActive(!_pauseMenu.activeSelf);
+            }
         }
     }
 }
