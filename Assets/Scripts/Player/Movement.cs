12543a8bd28306b8aa3f3e94e3a4bf89a9967ed0
Bengt Söderberg <bengt1982@gmail.com>
2024-11-28 09:59:12 +0100

    Remove unnecessary line of code

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -170,7 +170,6 @@ namespace Player
             if (Mathf.Approximately(currentVelocity.magnitude, 0))
             {
                 StartCoroutine(BounceRoutine());
-                IsBouncing = true;
                 currentVelocity = wallBounceOffFactor * normal;
             }
 
