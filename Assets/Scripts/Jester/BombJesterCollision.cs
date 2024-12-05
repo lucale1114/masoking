564bc778fd60a15aa35b7146061ba8ded06d2415
Legsinton <alexander_raberg@hotmail.com>
2024-12-05 15:50:38 +0100

    script

--- Assets/Scripts/Jester/BombJesterCollision.cs
+++ Assets/Scripts/Jester/BombJesterCollision.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BombJesterCollision : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+namespace Objects
+{
+    public class BombJesterCollision : MonoBehaviour
     {
-        
+        Rigidbody2D rb;
+        void Start()
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
