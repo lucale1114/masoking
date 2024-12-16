b384b4b825b19ad2dd97a722852d7fa6cb10e893
Legsinton <alexander_raberg@hotmail.com>
2024-12-16 12:23:04 +0100

    new

---
+++ Assets/SideScroallerKnife.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideScroallerKnife : MonoBehaviour
+{
+    public GameObject Knife;
+
+    public float minSpeed;
+    public float maxSpeed;
+    public float currentSpeed;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        currentSpeed = minSpeed;
+        GenerateSpike();
+    }
+
+    void GenerateSpike()
+    {
+        GameObject KnifeIns = Instantiate(Knife, transform.position, transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
