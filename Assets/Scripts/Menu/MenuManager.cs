78ef6ac7b95bb128aae38682d573028aba95a923
Luca A. <92426198+lucale1114@users.noreply.github.com>
2024-12-18 15:31:36 +0100

    Credits WIp

--- Assets/Scripts/Menu/MenuManager.cs
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -8,6 +8,9 @@ namespace Menu
     public class MenuManager : MonoBehaviour
     {
         Button exitButton;
+        GameObject[] credits;
+        GameObject creditsMenu;
+        GameObject currentCredits;
         private bool shaking;
 
         private void Awake()
@@ -16,6 +19,25 @@ namespace Menu
             GameObject.Find("TutorialBtn").GetComponent<Button>().onClick.AddListener(GameManager.LoadTutorial);
             exitButton = GameObject.Find("Exit").GetComponent<Button>();
             exitButton.onClick.AddListener(GameManager.Quit);
+
+            credits[0] = GameObject.Find("Credits1");
+            creditsMenu = GameObject.Find("Credits");
+            currentCredits = credits[0];
+            credits[1] = GameObject.Find("Credits2");
+            credits[2] = GameObject.Find("Credits3");
+            credits[1].SetActive(false);
+            credits[2].SetActive(false);
+            creditsMenu.SetActive(true);
+        }
+
+        public void NextButton()
+        {
+
+        }
+
+        public void BackButton()
+        {
+            //try()
         }
 
         public void Entered()
