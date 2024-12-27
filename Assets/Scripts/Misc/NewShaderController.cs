b25e8c8020c6e1db7f8bdf6ac534943a9ae4172d
Bengt Söderberg <bengt1982@gmail.com>
2024-12-27 09:24:28 +0100

    Move script

---
+++ Assets/Scripts/Misc/NewShaderController.cs
@@ -0,0 +1,55 @@
+using Player;
+using UnityEngine;
+
+namespace Misc
+{
+    public class NewShaderController : MonoBehaviour
+    {
+
+        private static readonly int Twirl = Shader.PropertyToID("_Twirl");
+        private static readonly int Speed = Shader.PropertyToID("_Speed");
+        private static readonly int MoveX = Shader.PropertyToID("_MoveX");
+        private static readonly int MoveY = Shader.PropertyToID("_MoveY");
+
+        private float baseTwirl;
+        private float baseSpeed;
+        private float xSpeed;
+        private float ySpeed;
+
+
+        private HeatSystem _heatSystem;
+        private Material _material;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _heatSystem = FindObjectOfType<HeatSystem>();
+            _material = GetComponent<MeshRenderer>().material;
+
+            _heatSystem.TakenDamage +=  Increase;
+
+            baseTwirl = _material.GetFloat(Twirl);
+            baseSpeed = _material.GetFloat(Speed);
+            xSpeed = _material.GetFloat(MoveX);
+            ySpeed = _material.GetFloat(MoveY);
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            var _twirl = _material.GetFloat(Twirl);
+            var _speed = _material.GetFloat(Speed);
+            var _moveX = _material.GetFloat(MoveX);
+            var _moveY = _material.GetFloat(MoveY);
+
+            _twirl = Mathf.Lerp(_twirl, baseTwirl, Time.deltaTime * 6);
+            _material.SetFloat(Twirl, _twirl);
+        }
+
+        public void Increase(float amount){
+            var _twirl = _material.GetFloat(Twirl);
+            _material.SetFloat(Twirl, _twirl + amount);
+        }
+    }
+}
--- Assets/Scripts/Misc/ShaderController.cs
+++ Assets/Scripts/Misc/ShaderController.cs
@@ -2,28 +2,30 @@ using System.Collections;
 using Player;
 using UnityEngine;
 
-public class ShaderController : MonoBehaviour
+namespace Misc
 {
-    private static readonly int Twirl = Shader.PropertyToID("_Twirl");
-    private static readonly int Speed = Shader.PropertyToID("_Speed");
-    private static readonly int MoveX = Shader.PropertyToID("_MoveX");
-    private static readonly int MoveY = Shader.PropertyToID("_MoveY");
-
-    [SerializeField] private float twirlMultiplier = 5;
-    [SerializeField] private float speedMultiplier = 5;
-    [SerializeField] private float moveXMultiplier = 5;
-    [SerializeField] private float moveYMultiplier = 5;
-    [SerializeField] private float duration = 1f;
-
-    private HeatSystem _heatSystem;
-    private Material _material;
-
-    private bool _inprogress;
-
-    private void Start()
+    public class ShaderController : MonoBehaviour
     {
-        _heatSystem = FindObjectOfType<HeatSystem>();
-        _material = GetComponent<MeshRenderer>().material;
+        private static readonly int Twirl = Shader.PropertyToID("_Twirl");
+        private static readonly int Speed = Shader.PropertyToID("_Speed");
+        private static readonly int MoveX = Shader.PropertyToID("_MoveX");
+        private static readonly int MoveY = Shader.PropertyToID("_MoveY");
+
+        [SerializeField] private float twirlMultiplier = 5;
+        [SerializeField] private float speedMultiplier = 5;
+        [SerializeField] private float moveXMultiplier = 5;
+        [SerializeField] private float moveYMultiplier = 5;
+        [SerializeField] private float duration = 1f;
+
+        private HeatSystem _heatSystem;
+        private Material _material;
+
+        private bool _inprogress;
+
+        private void Start()
+        {
+            _heatSystem = FindObjectOfType<HeatSystem>();
+            _material = GetComponent<MeshRenderer>().material;
 /*
         _heatSystem.TakenDamage += () =>
         {
@@ -34,29 +36,30 @@ public class ShaderController : MonoBehaviour
         };
 
         */
-    }
+        }
 
-    private IEnumerator ChangeRoutine()
-    {
-        _inprogress = true;
+        private IEnumerator ChangeRoutine()
+        {
+            _inprogress = true;
 
-        var twirl = _material.GetFloat(Twirl);
-        var speed = _material.GetFloat(Speed);
-        var moveX = _material.GetFloat(MoveX);
-        var moveY = _material.GetFloat(MoveY);
+            var twirl = _material.GetFloat(Twirl);
+            var speed = _material.GetFloat(Speed);
+            var moveX = _material.GetFloat(MoveX);
+            var moveY = _material.GetFloat(MoveY);
 
-        _material.SetFloat(Twirl, twirl * twirlMultiplier);
-        _material.SetFloat(Speed, speed * speedMultiplier);
-        _material.SetFloat(MoveX, moveX * moveXMultiplier);
-        _material.SetFloat(MoveY, moveY * moveYMultiplier);
+            _material.SetFloat(Twirl, twirl * twirlMultiplier);
+            _material.SetFloat(Speed, speed * speedMultiplier);
+            _material.SetFloat(MoveX, moveX * moveXMultiplier);
+            _material.SetFloat(MoveY, moveY * moveYMultiplier);
 
-        yield return new WaitForSeconds(duration);
+            yield return new WaitForSeconds(duration);
 
-        _material.SetFloat(Twirl, twirl);
-        _material.SetFloat(Speed, speed);
-        _material.SetFloat(MoveX, moveX);
-        _material.SetFloat(MoveY, moveY);
+            _material.SetFloat(Twirl, twirl);
+            _material.SetFloat(Speed, speed);
+            _material.SetFloat(MoveX, moveX);
+            _material.SetFloat(MoveY, moveY);
 
-        _inprogress = false;
+            _inprogress = false;
+        }
     }
 }
