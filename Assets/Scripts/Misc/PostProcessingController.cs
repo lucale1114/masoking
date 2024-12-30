8b785be8ad636334d6286409e0ab34ab680004df
Bengt Söderberg <bengt1982@gmail.com>
2024-12-30 15:36:34 +0100

    Add saturation control

--- Assets/Scripts/Misc/PostProcessingController.cs
+++ Assets/Scripts/Misc/PostProcessingController.cs
@@ -12,7 +12,11 @@ namespace Misc
         [SerializeField] private float vignetteEndIntensity = 0.4f;
         [SerializeField] private float vignetteDuration = 0.2f;
 
+        [SerializeField] private float saturationLowValue = -50f;
+        [SerializeField] private float saturationHighValue = 50f;
+
         private Vignette _vignette;
+        private ColorAdjustments _colorAdjustments;
         private HeatSystem _heatSystem;
 
         private void Start()
@@ -20,16 +24,23 @@ namespace Misc
             _heatSystem = FindObjectOfType<HeatSystem>();
 
             GetComponent<Volume>().profile.TryGet(out _vignette);
+            GetComponent<Volume>().profile.TryGet(out _colorAdjustments);
+
             _vignette.intensity.value = vignetteStartIntensity;
 
+            _heatSystem.HeatChanged += UpdateSaturation;
             _heatSystem.TakenDamage += Shake;
         }
 
-
-        private void Shake(float _){
-            StartCoroutine(ShakeRoutine());
+        private void UpdateSaturation(float heat)
+        {
+            _colorAdjustments.saturation.value = Mathf.Lerp(saturationLowValue, saturationHighValue, heat);
         }
 
+        private void Shake(float _)
+        {
+            StartCoroutine(ShakeRoutine());
+        }
 
         private IEnumerator ShakeRoutine()
         {
