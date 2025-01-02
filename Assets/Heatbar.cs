cceee2ef095fc60ecda8acc4da7aa7988198c95e
Legsinton <alexander_raberg@hotmail.com>
2025-01-02 15:32:37 +0100

    e

--- Assets/Heatbar.cs
+++ Assets/Heatbar.cs
@@ -5,6 +5,7 @@ namespace Player
     public class HealthBar : MonoBehaviour
     {
         [SerializeField] private Transform healthFill; // The filling portion of the health bar (Transform or Image)
+        [SerializeField] private SpriteRenderer healthBarRenderer; // Optional, for changing color based on health
 
         // Update the health bar based on the normalized heat value (0 to 1)
         public void UpdateHealthBar(float normalizedHeat)
