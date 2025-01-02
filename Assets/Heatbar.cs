35eb14ae71c571254aa868c5b0aef16119deafd6
Legsinton <alexander_raberg@hotmail.com>
2025-01-02 15:11:34 +0100

    new door

--- Assets/Heatbar.cs
+++ Assets/Heatbar.cs
@@ -5,7 +5,6 @@ namespace Player
     public class HealthBar : MonoBehaviour
     {
         [SerializeField] private Transform healthFill; // The filling portion of the health bar (Transform or Image)
-        [SerializeField] private SpriteRenderer healthBarRenderer; // Optional, for changing color based on health
 
         // Update the health bar based on the normalized heat value (0 to 1)
         public void UpdateHealthBar(float normalizedHeat)
@@ -16,28 +15,6 @@ namespace Player
                 healthFill.localScale = new Vector3(normalizedHeat, 1f, 1f); // Scale on the X axis
             }
 
-            // Optionally, change color based on the heat level (can use SpriteRenderer or UI Image color change)
-            if (normalizedHeat >= 1f) // Max heat
-            {
-                if (healthBarRenderer != null)
-                {
-                    healthBarRenderer.color = Color.red;
-                }
-            }
-            else if (normalizedHeat <= 0f) // Empty heat
-            {
-                if (healthBarRenderer != null)
-                {
-                    healthBarRenderer.color = Color.green;
-                }
-            }
-            else // Mid-range heat
-            {
-                if (healthBarRenderer != null)
-                {
-                    healthBarRenderer.color = Color.Lerp(Color.green, Color.red, normalizedHeat);
-                }
-            }
         }
     }
 
